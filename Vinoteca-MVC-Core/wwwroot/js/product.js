a3127bca0de5fd73a239d0bc1fc130b92c3a282b
pablofrers93 <pablofrers93@gmail.com>
2023-11-10 17:52:31 -0300

    delete product

--- Vinoteca-MVC-Core/wwwroot/js/product.js
+++ Vinoteca-MVC-Core/wwwroot/js/product.js
@@ -35,3 +35,34 @@ $(document).ready(function () {
     });
 
 });
+
+function Delete(url) {
+    console.log(url);
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                "url": url,
+                "type": 'DELETE',
+                "success": function (data) {
+                    console.log(data);
+                    if (data.success) {
+                        productTable.ajax.reload();
+                        toastr.success(data.message);
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+
+            });
+        }
+    })
+};
